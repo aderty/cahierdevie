425c84b512a85672bedb3646e1acafe9aea18b1d
Julien <julien@mezimail.com>
2013-10-08 00:38:39 +0200

    rt

--- js/filters.js
+++ js/filters.js
@@ -9,6 +9,7 @@ angular.module('myApp.filters', [])
     }])
 .filter('datesimple', function () {
     return function (date) {
+        if (!date) return "";
         var month = (date.getMonth() + 1);
         if (month < 10) {
             month = '0' + month;
