2204418f03242c2e9049f9ea7058fb4ccf8b1d97
Julien <julien@mezimail.com>
2013-11-07 00:51:46 +0100

    gf

--- js/controllers.js
+++ js/controllers.js
@@ -444,7 +444,7 @@ function EventCtrl($scope, $rootScope, navSvc, EnfantService, CahierService, Eve
         var options = {
             quality: 60,
             destinationType: Camera.DestinationType.FILE_URI, //Camera.DestinationType.DATA_URL,
-            sourceType: 0,      // 0:Photo Library, 1=Camera, 2=Saved Photo Album
+            sourceType: 1,      // 0:Photo Library, 1=Camera, 2=Saved Photo Album
             encodingType: 0,     // 0=JPG 1=PNG
             targetWidth: 1000,
             targetHeight: 1000,
